804a997ad234058d8f4e9b3589ac67a2653047b7
KZ-DE <nebulacom005@gmail.com>
2023-02-21 09:49:04 +0700

    lanjutkan

--- main.cpp
+++ main.cpp
@@ -4,10 +4,7 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-<<<<<<< HEAD
-    cout << "test" << endl;
-=======
+
     cout << "hello word" << endl;
->>>>>>> e187a957e4ee29c452603327646d65aafee8a44d
     return 0;
 }
